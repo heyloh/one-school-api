ed415e45f35d9ac6b1169ebc8b5bf608c3165302
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-04 23:51:42 -0300

    ✨ Add students migrations

---
+++ src/database/migrations/20201205023645-students.js
@@ -0,0 +1,52 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /* Create students */
+    return queryInterface.createTable('students', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      surname: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      age: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      weight: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      height: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /* Delete students table */
+    return queryInterface.dropTable('students');
+  },
+};
