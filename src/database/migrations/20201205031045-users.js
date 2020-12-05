3aeeabaaeeb1fb059210246125144c48e9f695c9
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-05 00:13:38 -0300

    ✨ Add users migration

---
+++ src/database/migrations/20201205031045-users.js
@@ -0,0 +1,41 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable arrow-body-style */
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /* Create users table */
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /* Delete users table */
+    return queryInterface.dropTable('users');
+  },
+};
