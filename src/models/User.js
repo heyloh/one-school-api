62bf8b06160d1f55d926e54df306740bb4703f19
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-05 00:32:52 -0300

    ✨ Add User model

---
+++ src/models/User.js
@@ -0,0 +1,48 @@
+import Sequelize, { Model } from 'sequelize';
+import bcryptjs from 'bcryptjs';
+
+export default class User extends Model {
+  static init(sequelize) {
+    super.init({
+      name: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [3, 70],
+            msg: 'Name field must be between 3 and 70 characters.',
+          },
+        },
+      },
+      email: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+        validate: {
+          isEmail: {
+            msg: 'Invalid email address.',
+          },
+        },
+      },
+      password_hash: {
+        type: Sequelize.STRING,
+        defaultValue: '',
+      },
+      password: {
+        type: Sequelize.VIRTUAL,
+        defaultValue: '',
+        validate: {
+          len: {
+            args: [6, 50],
+            msg: 'Password field must be between 6 and 50 characters.',
+          },
+        },
+      },
+    }, {
+      sequelize,
+    });
+    this.addHook('beforeSave', async (user) => {
+      user.password_hash = await bcryptjs.hash(user.password, 8);
+    });
+    return this;
+  }
+}
