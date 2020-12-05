50de1eda321acdd6fece2a3e02e61a02d84e876a
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-05 00:44:13 -0300

    ✨ Add route for user creation

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,14 @@ import express from 'express';
 
 /* Controllers */
 import HomeController from './controllers/HomeController';
+import UserController from './controllers/UserController';
 
 const routes = express.Router();
 
 /* Home */
 routes.get('/', HomeController.index);
+
+/* User */
+routes.post('/users', UserController.store);
 
 export default routes;
