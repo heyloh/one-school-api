fff4bab4a50d445840f6637e52bdc15858c1b6e1
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-04 23:12:46 -0300

    🔧 Add config files

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    "env": {
+        "es6": true,
+        "node": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 6,
+        "sourceType": "module"
+    },
+    "rules": {
+      "no-console": "off"
+    },
+    "parser": "babel-eslint"
+};
