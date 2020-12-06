ba09187a84ca9a32007425f8482828cf7bae686e
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 15:26:20 -0300

    ✨ Add users routes

--- src/routes.js
+++ src/routes.js
@@ -11,5 +11,9 @@ routes.get('/', HomeController.index);
 
 /* User */
 routes.post('/users', UserController.store);
+routes.get('/users', UserController.index);
+routes.get('/users/:id', UserController.show);
+routes.put('/users/:id?', UserController.update);
+routes.delete('/users/:id?', UserController.delete);
 
 export default routes;
