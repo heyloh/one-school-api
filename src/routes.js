e40d4f65809490f7936d6cc78e3a086ed8f5eb39
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 19:06:37 -0300

    ✨ Add LoginRequired middleware on students routes

--- src/routes.js
+++ src/routes.js
@@ -16,9 +16,9 @@ routes.post('/tokens', TokenController.store);
 /* Student */
 routes.get('/students', StudentController.index);
 routes.get('/students/:id', StudentController.show);
-routes.post('/students', StudentController.store);
-routes.put('/students/:id', StudentController.update);
-routes.delete('/students/:id', StudentController.delete);
+routes.post('/students', LoginRequired, StudentController.store);
+routes.put('/students/:id', LoginRequired, StudentController.update);
+routes.delete('/students/:id', LoginRequired, StudentController.delete);
 
 /* User */
 routes.post('/users', UserController.store);
