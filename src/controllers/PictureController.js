8a9b225638a92848418fe227ad08441b980b596e
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 20:00:45 -0300

    ✨ Store picture on db

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,18 +1,24 @@
 import multer from 'multer';
 import multerConfig from '../config/multer';
 
+import Picture from '../models/Picture';
+
 const upload = multer(multerConfig).single('picture');
 
 export default {
   async store(request, response) {
-    return upload(request, response, (err) => {
+    return upload(request, response, async (err) => {
       if (err) {
         return response.status(400).json({
           errors: [err.code],
         });
       }
 
-      return response.json(request.file);
+      const { originalname, filename } = request.file;
+      const { student_id } = request.body;
+      const picture = await Picture.create({ originalname, filename, student_id });
+
+      return response.json(picture);
     });
   },
 };
