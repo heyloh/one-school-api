7951e6509b6c4e3b2692361b0ca6a77658c3b7b3
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 20:01:11 -0300

    ✨ Add Picture model to models array

--- src/database/index.js
+++ src/database/index.js
@@ -4,9 +4,11 @@ import dbConfig from '../config/database';
 /* Models */
 import Student from '../models/Student';
 import User from '../models/User';
+import Picture from '../models/Picture';
 
-const models = [Student, User];
+const models = [Student, User, Picture];
 
 const connection = new Sequelize(dbConfig);
 
 models.forEach((model) => model.init(connection));
+models.forEach((model) => model.associate && model.associate(connection.models));
