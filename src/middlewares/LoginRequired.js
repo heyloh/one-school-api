5dee617d670fbdbb04449eaa3fe7f176badd6d6f
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 16:19:31 -0300

    ✨ Require login after email update

--- src/middlewares/LoginRequired.js
+++ src/middlewares/LoginRequired.js
@@ -2,8 +2,9 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 import jwt from 'jsonwebtoken';
+import User from '../models/User';
 
-export default (request, response, next) => {
+export default async (request, response, next) => {
   const { authorization } = request.headers;
 
   if (!authorization) {
@@ -17,6 +18,20 @@ export default (request, response, next) => {
   try {
     const data = jwt.verify(token, process.env.TOKEN_SECRET);
     const { id, email } = data;
+
+    const user = await User.findOne({
+      where: {
+        id,
+        email,
+      },
+    });
+
+    if (!user) {
+      return response.status(401).json({
+        errors: ['Invalid user.'],
+      });
+    }
+
     request.user_id = id;
     request.user_email = email;
     return next();
