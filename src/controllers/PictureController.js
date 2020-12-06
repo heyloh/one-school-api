2d08334fcbe05a01d4b5e2b0c4624da50687ea68
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 19:32:36 -0300

    ✨ Update PictureController with store method

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -1,5 +1,18 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('picture');
+
 export default {
   async store(request, response) {
-    return response.json({ message: 'ok' });
+    return upload(request, response, (err) => {
+      if (err) {
+        return response.status(400).json({
+          errors: [err.code],
+        });
+      }
+
+      return response.json(request.file);
+    });
   },
 };
