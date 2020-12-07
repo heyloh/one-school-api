48c6a8719b55a073faf52970606a41ce255a45ba
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 22:05:45 -0300

    🔧 Static config

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv';
 dotenv.config();
 import './database';
 import express from 'express';
+import { resolve } from 'path';
 import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 export default app;
