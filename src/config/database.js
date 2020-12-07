89967ba25b564cb8813a2a6721ed70dcd53b7c9a
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 22:48:49 -0300

    🚀 Deploy

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   dialect: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: process.env.DATABASE_PORT,
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PWD,
-  database: process.env.DATABASE,
+  url: process.env.DATABASE_URL,
   define: {
     timestamps: true,
     underscored: true,
