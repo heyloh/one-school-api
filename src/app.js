3f64f2a99ab56a3351b227326045cc951ffa7c49
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-07 17:36:13 -0300

    Revert "🐛"
    This reverts commit d7f684972d568ea66b948ee29bba3d9c930ecd0c.

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ import routes from './routes';
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
