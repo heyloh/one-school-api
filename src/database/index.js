7b60a390decafd5aa23c64eba4acb0d47d2b4f85
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 23:44:57 -0300

    🐛

--- src/database/index.js
+++ src/database/index.js
@@ -8,7 +8,11 @@ import Picture from '../models/Picture';
 
 const models = [Student, User, Picture];
 
-const connection = new Sequelize(dbConfig);
+const connection = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  protocol: 'postgres',
+  logging: true,
+});
 
 models.forEach((model) => model.init(connection));
 models.forEach((model) => model.associate && model.associate(connection.models));
