cd28f0a33ca7752758358adbc7e27ea8811445ba
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-07 18:11:13 -0300

    🚀 Deploy config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 import app from './app';
 
-const port = 3333;
+const port = process.env.PORT || 3333;
 
 app.listen(port, () => {
   console.log(`🚀 Server listening on port: ${port}`);
