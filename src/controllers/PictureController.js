41a83b6137554e2eeb30ef9e666d8dd51aef820c
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-06 21:55:02 -0300

    🥅 Catch errors on PictureController

--- src/controllers/PictureController.js
+++ src/controllers/PictureController.js
@@ -14,11 +14,17 @@ export default {
         });
       }
 
-      const { originalname, filename } = request.file;
-      const { student_id } = request.body;
-      const picture = await Picture.create({ originalname, filename, student_id });
+      try {
+        const { originalname, filename } = request.file;
+        const { student_id } = request.body;
+        const picture = await Picture.create({ originalname, filename, student_id });
 
-      return response.json(picture);
+        return response.json(picture);
+      } catch (e) {
+        return response.status(400).json({
+          errors: ['Student does not exist.'],
+        });
+      }
     });
   },
 };
