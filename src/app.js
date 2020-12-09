5334b13ca349ab38cdf0ad554dde3c3e8b829225
Lohana Torres <lt.leopoldo@gmail.com>
2020-12-09 15:25:11 -0300

    🔧 Add cors and helmet config

--- src/app.js
+++ src/app.js
@@ -3,14 +3,33 @@ import dotenv from 'dotenv';
 dotenv.config();
 import './database';
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import { resolve } from 'path';
 import routes from './routes';
+
+const whitelist = [
+  'https://one-school-api.herokuapp.com',
+  'http://localhost:3000',
+];
+
+const corsOptions = {
+  origin(origin, cb) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      cb(null, true);
+    } else {
+      cb(new Error('Not allowed by CORS.'));
+    }
+  },
+};
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(resolve(__dirname, '..', 'uploads')));
+app.use(cors(corsOptions));
+app.use(helmet());
 app.use(routes);
 
 export default app;
